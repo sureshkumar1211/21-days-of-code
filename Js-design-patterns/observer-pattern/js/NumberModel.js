993b4629f41244df46468a0f6be171b962c2430c
Sureshkumar Karuppusamy <suresh@arbaan.com>
2020-02-11 23:53:04 +0530

    javascript observer pattern initial commit

---
+++ Js-design-patterns/observer-pattern/js/NumberModel.js
@@ -0,0 +1,21 @@
+class NumberModel {
+	constructor() {
+		this.number = 0;
+		this.color = "red";
+		this.observers = [];
+	}
+	increament() {
+		let colors = ["green", "blue", "grey", "orange", "black"];
+		this.number++;
+		this.color = colors[Math.floor(Math.random() * colors.length)];
+		this.notifyObserver();
+	}
+	addObserver(o) {
+		this.observers.push(o);
+	}
+	notifyObserver() {
+		for (let observer of this.observers) {
+			observer.update(this);
+		}
+	}
+}
